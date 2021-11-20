b264a57a007db00013e36ab7a2ef8ee4b39b3ed3
ARAN <67265109+ARAN-ai-python@users.noreply.github.com>
2021-11-20 11:56:19 +0900

    Create BQ_number01.sql

--- preprocess/008_number/01/BQ_number01.sql
+++ preprocess/008_number/01/BQ_number01.sql
@@ -2,5 +2,5 @@ SELECT
     CAST((40000.0 / 3) AS INT64) AS v_int2, #INT2, INT4, INT8は無いらしい
     CAST((40000.0 / 3) AS INT) AS FLOAT64 #FLOAT4. FLOAT8は無いらしい
 FROM 
-    `awesome-preprocessing.work.reserve` AS reserve
+    `awesome-preprocessing.work.reserve_tb`
 LIMIT 1
