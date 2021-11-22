ec6411fe3d93afbefde306767bb2cc099540bf33
ARAN <67265109+ARAN-ai-python@users.noreply.github.com>
2021-11-22 18:57:13 +0900

    Update BQ_number07_a.sql

--- preprocess/008_number/07_a/BQ_number07_a.sql
+++ preprocess/008_number/07_a/BQ_number07_a.sql
@@ -2,13 +2,34 @@ SELECT
     *
 FROM 
     `awesome-preprocessing.work.production_missing_num_tb`
-WHERE
-    thickness != "None"
+WHERE 
+    thickness IS NOT NULL
 
 /*
-自分がbigquery上にデータをインポートしたら、NULLではなくNoneとなっていたので上記の処理で対応した。
-仮にNoneではなくNULLだったとしたら、前処理大全の方法で問題なく動作する。
+production_missing_num_tbをインポート時、スキーマを自動判定にしていたのだが、これが誤作動してthicknessのデータ型がSTRINGになってしまっていた。
+そのため、NULLがNoneとなっており、上記のクエリではエラーが出てしまう事態が発生した。
+これを機に、テーブルのスキーマ等を確認する癖をつけていきたい。
+尚、その後にCREATE TABLE AS SELECT~ で新しくproduction_missing_num_tbを定義しなおし、thicknessのデータ型をFLOATに直しておいた。
+以下がその時のクエリである。
 
+CREATE TABLE work.production_missing_num_tb
+AS
+   SELECT
+      type,
+      length,
+      CAST(thickness AS FLOAT64) AS thickness,
+      fault_flg
+   FROM
+      `awesome-preprocessing.work.production_missing_num` #同名のテーブルを重複して作れないので、一旦ダミーのテーブルとしてproduction_missing_numテーブルを作成した
+
+
+
+仮にNULLではなくNoneだったとしたら、以下の方法で対応できる。
+
+SELECT 
+    *
+FROM 
+    `awesome-preprocessing.work.production_missing_num_tb`
 WHERE
-    thickness is not NULL
+    thickness != "None"
 */
