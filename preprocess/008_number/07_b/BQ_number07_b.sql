24f459bd289f642a062be86e28994a247ac0a7bb
ARAN <67265109+ARAN-ai-python@users.noreply.github.com>
2021-11-22 18:39:20 +0900

    Update BQ_number07_b.sql

--- preprocess/008_number/07_b/BQ_number07_b.sql
+++ preprocess/008_number/07_b/BQ_number07_b.sql
@@ -1,19 +1,20 @@
 SELECT 
     type,
     length,
-    IF(thickness!="None" , CAST(thickness AS FLOAT64), 1.0) AS thickness,
+    COALESCE(thickness, 1.0) AS thickness,
     fault_flg
-FROM
+FROM 
     `awesome-preprocessing.work.production_missing_num_tb`
 
 /*
-thicknessの欠損値はNoneのため、COALESCE関数は使えない。
-代わりにIF文でthicknessがNoneかどうか判定して処理できた。
 IFNULL文も使えそうかも
 
-COALESCE関数を使って補完する場合
-COALESCE(thickness, 1.0) AS thickness
-
 IFNULL関数を使って補完する場合
-IFNULL(thickness, 1.0) AS thickness
+SELECT 
+    type,
+    length,
+    IFNULL(thickness, 1.0) AS thickness,
+    fault_flg
+FROM 
+    `awesome-preprocessing.work.production_missing_num_tb`
 */
