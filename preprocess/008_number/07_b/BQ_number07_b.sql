184341442e6475f26d09610716cffa7d6f43647c
ARAN <67265109+ARAN-ai-python@users.noreply.github.com>
2021-11-22 18:45:18 +0900

    Update BQ_number07_b.sql

--- preprocess/008_number/07_b/BQ_number07_b.sql
+++ preprocess/008_number/07_b/BQ_number07_b.sql
@@ -7,13 +7,22 @@ FROM
     `awesome-preprocessing.work.production_missing_num_tb`
 
 /*
-IFNULL文も使えそうかも
+IFNULL関数, IF関数も使えそうかも
 
 IFNULL関数を使って補完する場合
 SELECT 
     type,
     length,
     IFNULL(thickness, 1.0) AS thickness,
+    fault_flg
+FROM 
+    `awesome-preprocessing.work.production_missing_num_tb`
+    
+IF関数を使って補完する場合
+SELECT 
+    type,
+    length,
+    IF(thickness IS NULL, 1.0, thickness) AS thickness,
     fault_flg
 FROM 
     `awesome-preprocessing.work.production_missing_num_tb`
